99eb94bdc66df06a8ca464b2d7d747d15999ee39
DavidIsrael <davidisrael.rj@gmail.com>
2025-03-21 19:03:35 -0300

    getTasks

---
+++ api/task.js
@@ -0,0 +1,18 @@
+const moment = require('moment')
+
+module.exports = app => {
+    const getTasks = (req, res) => {
+        const date = req.query.date ? req.query.date
+            : moment().endOf('day').toDate()
+
+        app.db('tasks')
+            .where({ userId: req.user.id })
+            .where('estimateAt', '<=', date)
+            .orderBy('estimateAt')
+            .the(tasks => res.json(tasks))
+            .catch(err => res.status(400).json(err))
+    }
+
+   
+
+}
